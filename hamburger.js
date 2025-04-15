3dffec189eff992440f1f45efdb0547d15fdf275
binbows02 <sammy29032006@gmail.com>
2025-04-15 13:15:24 +0200

    mobile + rest van de hele kkr opdracht

--- hamburger.js
+++ hamburger.js
@@ -1,10 +1,19 @@
 
 let hamburgerHTML = document.querySelector(`.hamburgerJS`);
+let hamburgerOpen = document.querySelector(`.hamburgerOpenedJS`)
 
 function openHamburger() {
     if (hamburgerHTML.classList.contains(`hamburgerClosed`)) {
         hamburgerHTML.classList.remove(`hamburgerClosed`)
     } else {
         hamburgerHTML.classList.add(`hamburgerClosed`)
+    }
+}
+
+function hamburgerOpened() {
+    if (hamburgerOpen.classList.contains(`opacityDark`)) {
+        hamburgerOpen.classList.remove(`opacityDark`)
+    } else {
+        hamburgerOpen.classList.add(`opacityDark`)
     }
 }
